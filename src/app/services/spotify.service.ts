88bb6684061e1a2f3c121de5bcdfdbfa1df4d97b
abraxas <qqpp0090w@gmail.com>
2020-01-25 15:12:43 +0100

    simply query

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,7 +6,6 @@ import { map } from 'rxjs/operators';
 })
 export class SpotifyService {
 
-
   constructor(private http: HttpClient) {
     console.log("dentro spotify service");
   }
@@ -16,34 +15,33 @@ export class SpotifyService {
       .set('grant_type', 'client_credentials')
       .set('client_id', '0a0da8fa0e02421fbe2b2cf1fd0de39f')
       .set('client_secret', 'e344c565c2954fbdac62d4656f440e0a');
-
     return this.http.post("https://accounts.spotify.com/api/token", params);
   }
 
-  getNewReleases() {
 
+  getQuery(query: string, params?: any) {
     const headers = new HttpHeaders({
-      'Authorization': "Bearer BQAwSKUtoY95MOop-brPKMpHYbIzduu-OY-Xd_NZv4wneS1C7FZ22YsRY1CRV3DRJU7ERWCj_7ft0V6pYMM"
+      'Authorization': "Bearer BQAlBp3og95RcMsY15hW-y_XjknpPGvyImEs4s9Uf0hnHXSRbSSTdkhWMA8G0KH455LO5x3S51m-dAYrjSQ"
     });
-
-    return this.http.get("https://api.spotify.com/v1/browse/new-releases", { headers })
-    .pipe( map( data =>  data['albums'].items))
-
+    let options: any = { headers };
+    if (params) {
+      options = { headers, params };
+    }
+    return this.http.get(`https://api.spotify.com/${query}`, options);
   }
 
-  getArtist(artist:string, type:string) {
+  getNewReleases() {
+    return this.getQuery("v1/browse/new-releases")
+      .pipe(map(data => data['albums'].items))
+  }
+
+  getArtist(artist: string, type: string) {
     const params = new HttpParams()
       .set('q', artist)
-      .set('type', type);  
-    const headers = new HttpHeaders({
-      'Authorization':  "Bearer BQAwSKUtoY95MOop-brPKMpHYbIzduu-OY-Xd_NZv4wneS1C7FZ22YsRY1CRV3DRJU7ERWCj_7ft0V6pYMM"
-    });
-
-    return this.http.get("https://api.spotify.com/v1/search", {params, headers})
-    .pipe( map( data => {
-      return data['artists'].items;
-    }))
-
+      .set('type', type);
+    return this.getQuery("v1/search", params)
+      .pipe(map(data => {
+        return data['artists'].items;
+      }))
   }
-
 }
